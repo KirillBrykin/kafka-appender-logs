5afdf5f82c3050505b498b7d56e64d5872c6dcc5
brykis <kir.brykin@yandex.ru>
2023-10-04 12:55:46 +0300

    test

--- src/main/java/com/example/kafkaappenderlogs/controller/Test.java
+++ src/main/java/com/example/kafkaappenderlogs/controller/Test.java
@@ -12,17 +12,17 @@ public class Test {
 
     @GetMapping("/hello")
     public void hello() {
-        System.out.println("------Test.hello------");
-
-        System.out.println("---log.getName()---");
-        System.out.println(log.getName());
-        System.out.println("---log.getClass()---");
-        System.out.println(log.getClass());
+//        System.out.println("------Test.hello------");
+//
+//        System.out.println("---log.getName()---");
+//        System.out.println(log.getName());
+//        System.out.println("---log.getClass()---");
+//        System.out.println(log.getClass());
 
         log.debug("---debug Hello!---");
         log.info("---info Hello!---");
         log.warn("---warn Hello!---");
         log.error("---error Hello!---");
-        throw new RuntimeException("---RuntimeException Hello!---");
+//        throw new RuntimeException("---RuntimeException Hello!---");
     }
 }

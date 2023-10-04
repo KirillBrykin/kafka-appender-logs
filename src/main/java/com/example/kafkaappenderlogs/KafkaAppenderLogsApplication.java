8c99f4ee692dd7dd0fdb73ad4506f1d091b3d6d1
Брыкин Кирилл Игоревич <brykin_ki@corp.tander.ru>
2023-10-04 11:30:19 +0300

    Initial commit

---
+++ src/main/java/com/example/kafkaappenderlogs/KafkaAppenderLogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaappenderlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAppenderLogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAppenderLogsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kafkaappenderlogs/controller/Test.java
@@ -0,0 +1,28 @@
+package com.example.kafkaappenderlogs.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/test")
+public class Test {
+
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("------Test.hello------");
+
+        System.out.println("---log.getName()---");
+        System.out.println(log.getName());
+        System.out.println("---log.getClass()---");
+        System.out.println(log.getClass());
+
+        log.debug("---debug Hello!---");
+        log.info("---info Hello!---");
+        log.warn("---warn Hello!---");
+        log.error("---error Hello!---");
+        throw new RuntimeException("---RuntimeException Hello!---");
+    }
+}
